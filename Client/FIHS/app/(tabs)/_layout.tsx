556d7b933430f1fcb1ad364e347d3cf62fb7806d
yousefHelly <yousef.helly@gmail.com>
2024-02-09 15:14:08 +0200

    feat: improve header & add animations

--- Client/FIHS/app/(tabs)/_layout.tsx
+++ Client/FIHS/app/(tabs)/_layout.tsx
@@ -1,38 +1,27 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Stack, Tabs } from 'expo-router';
+import { Tabs } from 'expo-router';
 import Colors from '@/constants/Colors';
-
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-import { View } from '@/components/Themed';
-import { Image } from 'expo-image';
-import { StyleSheet } from 'react-native';
-import  Logo  from '../../assets/images/Logofinal.png';
-import  LogoBG  from '../../assets/images/logoBG.jpg';
+import TabsHeader from '@/components/layout/TabsHeader';
+import { Feather } from '@expo/vector-icons';
+import { useTabHeaderHeight } from '@/components/layout/useTabHeaderHeight';
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
-  return <FontAwesome size={30} {...props} />;
+  return <FontAwesome size={30} {...props}/>;
 }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+  const colorScheme = useColorScheme()
   return (
-   
-    
- 
-    <Tabs 
-      
-      screenOptions={{
-        headerStyle:{
-          height:60
-        },
-        
+    <Tabs       
+      screenOptions={{   
         tabBarStyle:{
+          flexDirection:'row-reverse',
           backgroundColor:'#fff',
           paddingTop:10,
           paddingBottom:4,
@@ -47,82 +36,38 @@ export default function TabLayout() {
           position:'relative',
           display:'flex'
         },
-             headerLeft: () => (
-                <View>
-
-                {/* <Button
-                  onPress={() => alert('This is a button!')}
-                  /> */}
-                 
-                <Image
-        style={styles.image}
-        source={Logo}
-        placeholder={"blurhash"}
-        contentFit="cover"
-        transition={1000}
-        />
-       
-        </View>
-              ),
-        headerBackground:()=>(    
-          <View    style={styles.BG1} >
-            <Image
-            style={styles.BG}
-              source={LogoBG}
-              placeholder={"blurhash"}
-              contentFit="cover"
-              transition={1000}
-            />
-          </View>
-        ),
+        header:({layout, navigation, options, route})=><TabsHeader/>,
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
       }}>
-      <Tabs.Screen
-    
-        name="four"
+        <Tabs.Screen
+        name="index"
         options={{
           title: '',
-          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
-          // headerRight: () => (
-          //   <Link href="/modal" asChild>
-          //     <Pressable>
-          //       {({ pressed }) => (
-          //         <FontAwesome
-          //         name="info-circle"
-          //           size={25}
-          //           color={Colors[colorScheme ?? 'dark'].text}
-          //           style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-          //           />
-          //           )}
-          //     </Pressable>
-          //   </Link>
-          // ),
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
         }}
-      />
+        />
       <Tabs.Screen
-        name="two"
+        name="three"
         options={{
           title: '',
           tabBarItemStyle:{
             marginRight:25
           },
-          tabBarIcon: ({ color }) => <TabBarIcon name="leaf" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="heart" color={color} />,
         }}
         />
-        <Tabs.Screen
-        
+      <Tabs.Screen
         name="scan"
-        
         options={{
           title: '',
           tabBarItemStyle:{
             position:'absolute',
-            left:'48%',
+            right:'50%',
             top:'-50%', 
-            transform:'translateX(-30px) translateY(-15px) ',
+            transform:'translateX(-32.5px) translateY(-15px) ',
             zIndex:50,
             width:65,
             height:65,
@@ -137,47 +82,26 @@ export default function TabLayout() {
           tabBarIconStyle:{
           marginTop:15
           },
-          tabBarIcon: ({ color }) => <TabBarIcon name="barcode" color={"white"} />,
+          tabBarIcon: ({ color }) => <Feather name="camera" color={"white"} size={28} />,
         }}
-        />
+      />     
       <Tabs.Screen
-        name="three"
+        name="two"
         options={{
           title: '',
           tabBarItemStyle:{
-            marginLeft:25
+            marginLeft:40
           },
-          tabBarIcon: ({ color }) => <TabBarIcon name="heart" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="leaf" color={color} />,
         }}
         />
       <Tabs.Screen
-        name="index"
+        name="four"
         options={{
           title: '',
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
         }}
-        />
+      />
     </Tabs>
   );
 }
-const styles = StyleSheet.create({
-  image: {
-    position:"absolute"
-    ,
-    bottom:"80%",
-width:150,
-height:70,
-backgroundColor:"",
-  },
-  BG: {
-width:500,
-height:"100%",
-},
-BG1: {
-  width:"100%",
-  height:"100%",
-  // transform:"translateY(-20px)",
-
-  },
-  
-});
--- Client/FIHS/app/(tabs)/index.tsx
+++ Client/FIHS/app/(tabs)/index.tsx
@@ -1,16 +1,50 @@
-import { StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
 import Fontisto from '@expo/vector-icons/Fontisto'
 import { Fab } from '@gluestack-ui/themed';
+// import { useNavigation } from 'expo-router';
+import { useTabHeaderHeight } from '@/components/layout/useTabHeaderHeight';
+import { useDerivedValue } from 'react-native-reanimated';
+import TabsPageContainer from '@/components/layout/TabsPageContainer';
 export default function TabOneScreen() {
-  return <>
-    <View style={styles.container}>
-      <Fab>
-        <Fontisto name='reddit'  size={32} color={'#fff'}/>
-      </Fab>
-    </View>
-
-    </>
+  const {heightWithoutSearch, heightWithSearch} = useTabHeaderHeight()  
+  return <TabsPageContainer>
+      <Text style={styles.title}>shhhs</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+      <Text style={styles.title}>hi</Text>
+    </TabsPageContainer>
   ;
 }
 
