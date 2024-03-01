75a5ca473f856d988cda3e613b29171e90255815
yousefHelly <yousef.helly@gmail.com>
2024-03-01 07:03:18 +0200

    feat: implement login hooks & api integration with backend

--- Client/FIHS/app/(auth)/login/index.tsx
+++ Client/FIHS/app/(auth)/login/index.tsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react'
 import { useRouter } from 'expo-router'
-import { Button, ButtonText,View ,Text, HStack, VStack, Input, ImageBackground,} from '@gluestack-ui/themed'
+import { Button, ButtonText,View ,Text, HStack, VStack, Input, ImageBackground, FormControl,} from '@gluestack-ui/themed'
 import { TextInput } from 'react-native-gesture-handler'
 import { SafeAreaView, TouchableOpacity} from 'react-native'
-import { Ionicons } from '@expo/vector-icons'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Image } from 'expo-image'
-import useSession from '@/hooks/state/useSession'
 import { StatusBar } from 'expo-status-bar'
 import Logo from '@/components/layout/Logo'
 import useLogin from '@/hooks/useLogin'
+import { Formik } from 'formik'
+import { Login, LoginSchema } from './../../../models/Login';
 type Props = {}
 const LoginPage = (props: Props) => {
   const [isPasswordShown,setIsPasswordShown]=useState(false);
   const router = useRouter()
   const login = useLogin()
-  const {setLoading} = useSession()
   return (
     <ImageBackground
     style={{
@@ -59,131 +59,169 @@ const LoginPage = (props: Props) => {
       marginHorizontal:15,
     }}
     >
-      <Text textAlign='center' color='$white' fontSize={25} fontWeight='800' pt={30} pb={10}>تسجيل الدخول</Text>
-      <View>
-        <Text fontSize={18} fontWeight='400' color='$white' marginVertical={5} marginRight={15} >البريد الالكتروني </Text>
-        <View style={{
-        width:"92%",   
-        marginRight:12,
-        marginLeft:12,
-        height:48,
-        borderColor:"#fff",
-        borderWidth:1,
-        borderTopEndRadius:10,
-        borderBottomStartRadius:10,
-        alignItems:"center",
-        justifyContent:"center",
-        backgroundColor:"#fff"
-        }}>
-        <TextInput
-        style={{
-          width:"100%",
-          textAlign:"right",
-          padding:8,
+      <Formik<LoginSchema>
+        initialValues={{
+          email:'',
+          password:''
         }}
-        placeholder='ادخل البريد الالكتروني '
-        />
-        </View>
-      </View>
-      <View marginTop={20}>
-        <Text fontSize={18} fontWeight='400' color='$white' marginVertical={5} marginRight={12}> كلمه المرور </Text>
-        <View style={{
-          marginRight:12,
-          marginLeft:12,
-        width:"92%",
-        height:48,
-        borderColor:"#fff",
-        borderWidth:1,
-        borderTopEndRadius:10,
-        borderBottomStartRadius:10,
-        alignItems:"center",
-        justifyContent:"center",
-        paddingLeft:22,
-        backgroundColor:"#fff"
+        validationSchema={Login}
+      onSubmit={
+        (vals)=>{
+          login.mutate((vals))
+        }
+      }
+      >
+        {
+          ({ handleBlur, setFieldValue, isValid, errors, submitForm})=>{
+            return <FormControl>
+              <Text textAlign='center' color='$white' fontSize={25} fontWeight='800' pt={30} pb={10}>تسجيل الدخول</Text>
+              <View>
+                <Text fontSize={18} fontWeight='400' color='$white' marginVertical={5} marginRight={15} >البريد الالكتروني </Text>
+                <View style={{
+                width:"92%",   
+                marginRight:12,
+                marginLeft:12,
+                height:48,
+                borderColor:"#fff",
+                borderWidth:1,
+                borderTopEndRadius:10,
+                borderBottomStartRadius:10,
+                alignItems:"center",
+                justifyContent:"center",
+                backgroundColor:"#fff"
+                }}>
+                <TextInput
+                style={{
+                  width:"100%",
+                  textAlign:"left",
+                  padding:8,
+                }}
+                onBlur={handleBlur('email')}
+                onChangeText={(e)=>setFieldValue('email', e)}
+                />
+                </View>
+                {
+                  errors.email&& 
+                  <HStack gap={'$1'} my={'$1.5'} mx={'$3'}>
+                    <Text size='xs' color='$textLight200'>
+                      <FontAwesome name='exclamation-circle'/>
+                    </Text>
+                    <Text size='xs' color='$textLight200'>{errors.email}</Text>
+                  </HStack>
+                }
+              </View>
+              <View marginTop={10}>
+                <Text fontSize={18} fontWeight='400' color='$white' marginVertical={5} marginRight={12}> كلمه المرور </Text>
+                <View style={{
+                  marginRight:12,
+                  marginLeft:12,
+                  width:"92%",
+                  height:48,
+                  borderColor:"#fff",
+                  borderWidth:1,
+                  borderTopEndRadius:10,
+                  borderBottomStartRadius:10,
+                  alignItems:"center",
+                  justifyContent:"center",
+                  paddingLeft:22,
+                  backgroundColor:"#fff"
+                }}
+                >
+                <TextInput
+                style={{
+                  width:"100%",
+                  textAlign:"left",
+                  padding:8,          
+                }}
+                secureTextEntry={isPasswordShown}
+                onBlur={handleBlur('password')}
+                onChangeText={(e)=>setFieldValue('password', e)}
+                />
+                <TouchableOpacity
+                onPress={()=>setIsPasswordShown(!isPasswordShown)}
+                style={{
+                  position:"absolute",
+                  left:12
+                }}>
+                  {isPasswordShown==true?
+                  (
+                    
+                    <Ionicons name='eye-off' size={24}/>
+                    ):(
+                      
+                      <Ionicons name='eye' size={24}/>
+                  )
+                  }
+                </TouchableOpacity>
+                </View>
+                {
+                  errors.password&& 
+                  <HStack gap={'$1'} my={'$1.5'} mx={'$3'}>
+                    <Text size='xs' color='$textLight200'>
+                      <FontAwesome name='exclamation-circle'/>
+                    </Text>
+                    <Text size='xs' color='$textLight200'>{errors.password}</Text>
+                  </HStack>
+                }
+              </View>
+              <Text marginHorizontal={8} color='#fff' fontSize={12} mx={'$3'}>هل نسيت كلمه المرور؟</Text>
+              <HStack 
+                justifyContent='center'
+                marginVertical={10}
+                >
+              <Button w={130}
+                h={"$12"}
+                backgroundColor='$white'
+                mb={20}
+                borderBottomStartRadius={10}
+                borderTopEndRadius={10}
+                onPress={isValid?submitForm:undefined}
+                >
+                <ButtonText color='#000' >تسجيل الدخول </ButtonText>
 
-        }}>
-        <TextInput
-        style={{
-          width:"100%",
-          textAlign:"right",
-          padding:8,          
-        }}
-        secureTextEntry={isPasswordShown}
-        placeholder=' ادخل كلمه المرور '
-        />
-        <TouchableOpacity
-        onPress={()=>setIsPasswordShown(!isPasswordShown)}
-        style={{
-          position:"absolute",
-          left:12
-        }}>
-          {isPasswordShown==true?
-          (
-            
-            <Ionicons name='eye-off' size={24}/>
-            ):(
-              
-              <Ionicons name='eye' size={24}/>
-          )
+              </Button>
+              </HStack>
+              <View
+                flexDirection='row'
+                alignItems='center'
+                my={2}
+                >
+                  <View
+                  style={{
+                    flex:1,
+                    height:1,
+                    backgroundColor:"#fff",
+                    marginHorizontal:10,
+                    
+                  }}/>
+                    <Text style={{fontSize:14 ,color:"#fff"}}>او </Text>
+                    <View
+                  style={{
+                    flex:1,
+                    height:1,
+                    backgroundColor:"#fff",
+                    marginHorizontal:10
+                  }}/>
+
+              </View>
+              <HStack justifyContent='center' gap={20}  pt={25} >
+                  <View bgColor='#fff' borderRadius={15} w={55} h={55} style={{justifyContent:"center",alignItems:"center"}} >
+                    <Image style={{width:30,height:30,}} source={require("@/assets/images/google-icon.png")}/>
+                  </View>
+                  <View bgColor='#fff' borderRadius={15} w={55} h={55} style={{justifyContent:"center",alignItems:"center"}} >
+                    <Ionicons name='logo-facebook' color="#0866FF"  size={40} />
+                  </View>
+              </HStack>
+              <Text textAlign='center' color='#e5e5e5' fontSize={14} fontWeight='800' pt={20}> 
+                <Text color='$textLight50' fontSize={14} underline fontWeight='800' onPress={()=>router.push('/(auth)/register')}>
+                لا تمتلك حساباً؟
+                </Text>
+              </Text>
+            </FormControl>
           }
-        </TouchableOpacity>
-        </View>
-        <Text marginHorizontal={8} color='#fff' fontSize={12} marginRight={15} onPress={()=>router.push('/(auth)/resetPassword')}>هل نسيت كلمه المرور؟</Text>
-      </View>
-      <HStack 
-        justifyContent='center'
-        marginVertical={10}
-        
-        >
-
-      <Button w={130}
-        h={"$12"}
-        backgroundColor='$white'
-        mb={20}
-        borderBottomStartRadius={10}
-        borderTopEndRadius={10}
-        onPress={()=>setLoading(true)}>
-        <ButtonText color='#000' >تسجيل الدخول </ButtonText>
-
-      </Button>
-      </HStack>
-        <View
-        flexDirection='row'
-        alignItems='center'
-        my={2}
-        >
-          <View
-          style={{
-            flex:1,
-            height:1,
-            backgroundColor:"#fff",
-            marginHorizontal:10,
-            
-          }}/>
-            <Text style={{fontSize:14 ,color:"#fff"}}>او </Text>
-            <View
-          style={{
-            flex:1,
-            height:1,
-            backgroundColor:"#fff",
-            marginHorizontal:10
-          }}/>
-
-        </View>
-        <HStack justifyContent='center' gap={20}  pt={25} >
-          <View bgColor='#fff' borderRadius={15} w={55} h={55} style={{justifyContent:"center",alignItems:"center"}} >
-            <Image style={{width:30,height:30,}} source={require("@/assets/images/google-icon.png")}/>
-          </View>
-          <View bgColor='#fff' borderRadius={15} w={55} h={55} style={{justifyContent:"center",alignItems:"center"}} >
-            <Ionicons name='logo-facebook' color="#0866FF"  size={40} />
-          </View>
-        </HStack>
-        <Text textAlign='center' color='#e5e5e5' fontSize={14} fontWeight='800' pt={20}> 
-          <Text color='$textLight50' fontSize={14} underline fontWeight='800' onPress={()=>router.push('/(auth)/register')}>
-          لا تمتلك حساباً؟
-          </Text>
-        </Text>
-      </View>
+        }
+      </Formik>
+    </View>
     </SafeAreaView>
     </ImageBackground>
   )
