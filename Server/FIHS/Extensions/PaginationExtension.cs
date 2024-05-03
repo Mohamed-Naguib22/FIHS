04ebe7db42848eb5f16feafcc35e5a2619590084
Mohamed-Naguib22 <mohamednageb20172@gmail.com>
2024-05-03 20:34:11 +0300

    Gemini API Integration Testing

--- Server/FIHS/Extensions/PaginationExtension.cs
+++ Server/FIHS/Extensions/PaginationExtension.cs
@@ -2,7 +2,7 @@
 
 public static class PaginationExtension
 {
-    public static (IEnumerable<T> result, int? nextPage) Paginate<T>(this IEnumerable<T> source, int offset, int limit) where T : class
+    public static  (IEnumerable<T> result, int? nextPage) Paginate<T>(this IEnumerable<T> source, int offset, int limit) where T : class
     {
         if (offset <= 0)
             offset = 1;
