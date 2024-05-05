f6af746678f2f8532c2395896ea43fc9f161921f
Alaa1554 <alakhshgv@gmail.com>
2024-05-05 07:33:24 +0300

    Comment Final Final Version

--- Server/FIHS/Controllers/CommentController.cs
+++ Server/FIHS/Controllers/CommentController.cs
@@ -14,15 +14,15 @@ namespace FIHS.Controllers
             _commentServices = commentServices;
         }
         [HttpPost("AddComment")]
-        public async Task<IActionResult> AddComment([FromQuery]AddCommentsDto addCommentsDto)
+        public async Task<IActionResult> AddComment([FromBody]AddCommentsDto addCommentsDto)
         {
             addCommentsDto.refreshToken = GetRefreshToken();
-            if ( addCommentsDto.refreshToken == null ) return BadRequest("حدث خطأ ما");
-            var result =   await _commentServices.AddCommentAsync(addCommentsDto);
-            return result ? Ok("تمت اضافة التعليق بنجاح") : BadRequest("حدث خطأ ما");
+            if (addCommentsDto.refreshToken == null ) return NotFound("لم يتم العثور علي اي مستخدم");
+            var result =  await _commentServices.AddCommentAsync(addCommentsDto);
+            return string.IsNullOrEmpty(result) ? Ok("تمت اضافة التعليق بنجاح") : BadRequest(result);
         }
         [HttpGet("GetAllComments")]
-        public async Task<IActionResult> GetAllComments( int entityId, string entityType)
+        public async Task<IActionResult> GetAllComments(int entityId, string entityType)
         {
             var result = await _commentServices.GetAllEntityComments(entityId, entityType);
             return Ok(result);
@@ -31,15 +31,15 @@ namespace FIHS.Controllers
         public async Task<IActionResult> EditComment([FromBody]AddCommentsDto commentDto, int Id)
         {
             commentDto.refreshToken = GetRefreshToken();
-            if (commentDto.refreshToken == null) return BadRequest("حدث خطأ ما");
-            var result = _commentServices.EditCommentAsync(Id,commentDto);
-            return result? Ok("تم تعديل التعليق بنجاح"):BadRequest("حدث خطأ ما");
+            if (commentDto.refreshToken == null) return NotFound("لم يتم العثور علي اي مستخدم");
+            var result =await _commentServices.EditCommentAsync(Id,commentDto);
+            return string.IsNullOrEmpty(result) ? Ok("تم تعديل التعليق بنجاح"):BadRequest(result);
         }
         [HttpDelete("DeleteComment/{id}")]
         public async Task<IActionResult> DeleteComment(int id)
         {
-            await _commentServices.DeleteCommentAsync(id);
-            return Ok("تم حذف التعليق بنجاح");
+           var result= await _commentServices.DeleteCommentAsync(id);
+           return result? Ok("تم حذف التعليق بنجاح") : NotFound("لم يتم العثور علي اي تعليق");
         }
         private string GetRefreshToken()
         {
