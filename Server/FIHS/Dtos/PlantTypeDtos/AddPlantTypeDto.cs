4ce9fd0fd2d3403f355596a9cc36a49beb73d6ff
Mohamed-Naguib22 <mohamednageb20172@gmail.com>
2024-06-03 21:17:59 +0300

    feat: Implement add and delete plant type

---
+++ Server/FIHS/Dtos/PlantTypeDtos/AddPlantTypeDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FIHS.Dtos.PlantTypeDtos
+{
+    public class AddPlantTypeDto
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        [Required, MaxLength(100)]
+        public string HeightRange { get; set; }
+        [Required, MaxLength(100)]
+        public string SpreadRange { get; set; }
+        [Required, MaxLength(100)]
+        public string LifeCycle { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
